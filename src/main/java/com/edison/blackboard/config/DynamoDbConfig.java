8bf0f1f54a2ba42e73452be7ba8aa3d13cec5c77
EdisonCat <edisonguocat@gmail.com>
2020-02-12 22:25:55 -0800

    update dynamodb config

---
+++ src/main/java/com/edison/blackboard/config/DynamoDbConfig.java
@@ -0,0 +1,39 @@
+package com.edison.blackboard.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Value("${amazon.access.key}")
+    private String awsAccessKey;
+
+    @Value("${amazon.access.secret-key}")
+    private String awsSecretKey;
+
+    @Value("${amazon.region}")
+    private String awsRegion;
+
+    @Value("${amazon.end-point.url}")
+    private String awsDynamoDBEndPoint;
+
+    @Bean
+    public DynamoDBMapper mapper() {
+        return new DynamoDBMapper(amazonDynamoDB());
+    }
+
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsDynamoDBEndPoint, awsRegion))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
+                .build();
+    }
+}
