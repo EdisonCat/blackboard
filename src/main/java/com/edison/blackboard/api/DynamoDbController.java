a9e522e498fec5f0efbb80d8ce04f20a7f78347b
EdisonCat <edisonguocat@gmail.com>
2020-02-12 23:44:04 -0800

    DynamoDbController.java created

---
+++ src/main/java/com/edison/blackboard/api/DynamoDbController.java
@@ -0,0 +1,9 @@
+package com.edison.blackboard.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/blackboard/dynamodb")
+@RestController
+public class DynamoDbController {
+}
