0af16709d90ebe8804b9864695524f56e3c87843
EdisonCat <edisonguocat@gmail.com>
2020-03-18 04:20:51 -0700

    update DynamoDbAnnouncementService.java

--- src/main/java/com/edison/blackboard/service/DynamoDbAnnouncementService.java
+++ src/main/java/com/edison/blackboard/service/DynamoDbAnnouncementService.java
@@ -1,7 +1,67 @@
 package com.edison.blackboard.service;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import com.edison.blackboard.model.Announcement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class DynamoDbAnnouncementService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DynamoDBMapper.class);
+    private final DynamoDbBoardService dynamoDbBoardService;
+    private final DynamoDBMapper mapper;
+
+    @Autowired
+    public DynamoDbAnnouncementService(DynamoDBMapper mapper, DynamoDbBoardService dynamoDbBoardService) {
+        this.mapper = mapper;
+        this.dynamoDbBoardService = dynamoDbBoardService;
+    }
+
+    public void insertAnnouncement(Announcement announcement) {
+        mapper.save(announcement);
+    }
+
+    public Announcement getAnnouncementById(UUID id) {
+        return mapper.load(Announcement.class, id);
+    }
+
+    public void updateAnnouncement(Announcement announcement) {
+        try {
+            mapper.save(announcement, buildDynamoDbSaveExpression(announcement));
+        }
+        catch (ConditionalCheckFailedException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    public List<Announcement> getAllAnnouncements() {
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
+        return mapper.scan(Announcement.class, scanExpression);
+    }
+
+    public void deleteAnnouncement(Announcement announcement) {
+        mapper.delete(announcement);
+    }
+
+    public DynamoDBSaveExpression buildDynamoDbSaveExpression(Announcement announcement) {
+        DynamoDBSaveExpression saveExpression = new DynamoDBSaveExpression();
+        Map<String, ExpectedAttributeValue> expectedAttributeValueMap = new HashMap<>();
+        expectedAttributeValueMap.put("announcementid", new ExpectedAttributeValue(new AttributeValue(announcement.getAnnouncementId().toString())));
+        saveExpression.setExpected(expectedAttributeValueMap);
+        return saveExpression;
+    }
+
 }
