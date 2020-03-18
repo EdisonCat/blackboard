40f4f0475f9a8ff569853769895bb22e7d9522a2
EdisonCat <edisonguocat@gmail.com>
2020-03-18 04:33:26 -0700

    add insert method in DynamoDbBoardService.java

--- src/main/java/com/edison/blackboard/service/DynamoDbBoardService.java
+++ src/main/java/com/edison/blackboard/service/DynamoDbBoardService.java
@@ -1,11 +1,17 @@
 package com.edison.blackboard.service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 import com.edison.blackboard.model.Board;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DynamoDbBoardService {
@@ -18,5 +24,17 @@ public class DynamoDbBoardService {
     public DynamoDbBoardService(DynamoDBMapper mapper, DynamoDbAnnouncementService dynamoDbAnnouncementService) {
         this.mapper = mapper;
         this.dynamoDbAnnouncementService = dynamoDbAnnouncementService;
+    }
+
+    public void insertBoard(Board board) {
+        mapper.save(board);
+    }
+
+    public DynamoDBSaveExpression buildDynamoDBSaveExpression(Board board) {
+        DynamoDBSaveExpression saveExpression = new DynamoDBSaveExpression();
+        Map<String, ExpectedAttributeValue> expectedAttributeValueMap = new HashMap<>();
+        expectedAttributeValueMap.put("boardid", new ExpectedAttributeValue(new AttributeValue(board.getBoardId().toString())));
+        saveExpression.setExpected(expectedAttributeValueMap);
+        return saveExpression;
     }
 }
